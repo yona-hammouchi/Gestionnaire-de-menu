26d823f3f99b473af7f859415da68985151ce996
Your Name <marvin.delansorne@laplateforme.io>
2025-01-27 16:25:41 +0100

    Index partie 1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cook & Share</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <img src="./img/navbar.png" alt ="navbar">
+        </nav>
+    </header>
+    <main>
+    </main>
+    <footer>
+    </footer>
+</body>
+</html>
