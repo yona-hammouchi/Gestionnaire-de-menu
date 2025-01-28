bf3880dd5039ed58fa3b739c7923efd0b641966a
Your Name <marvin.delansorne@laplateforme.io>
2025-01-28 13:59:40 +0100

    Changement .html à .php

--- index.php
+++ index.php
@@ -3,18 +3,76 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./global.css">
     <title>Cook & Share</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <header>
         <nav>
-            <img src="./img/navbar.png" alt ="navbar">
+            <ul class = "navbar">
+                <li>
+                    <div class="logo_acceuil">
+                        <img src="./img/logo_cook_&_share.png" alt="logo_cook&share" height="100px">
+                    </div>
+                </li>
+                <li>
+                    <div class ="logo_navbar">
+                        <img src="./img/logo profile.png" alt="logo_profile">
+                    </div>
+                </li>
+            </ul>
         </nav>
     </header>
     <main>
+        <section class="main_index">
+            <div class = "img_acceuil">
+                <img src="./img/img_acceuil.png" alt="photo_acceuil" >
+            </div>
+            <div class="texte_index">
+                    <h1>
+                        Bienvenue sur Cook & Share ! 
+                    </h1>
+                    <p>
+                        Notre site de partage de recette entre restaurateur
+                    </p>
+            </div>
+        </section>
+        <h1>
+            Les recettes les plus vues
+        </h1>
+        
+        <section class="photo_recettes">
+            <img src="./img/photo_recette1.png" alt="photo_recette1"height="476" width="300">
+            <div class="photo_placement">
+                <img src="./img/photo_recette2.png" alt="photo_recette2" height="476" width="300">
+            </div>
+            <img src="./img/photo_recette3.png" alt="photo_recette3"height="476" width="300">
+            <div class="photo_placement">
+                <img src="./img/photo_recette4.png" alt="photo_recette4"height="476" width="300">
+            </div>
+        </section>
+        
     </main>
     <footer>
+        <section class="footer">
+            <div>
+                <p>
+                    Contact
+                </p>
+            </div>
+            <div>
+                Connexion
+            </div>
+            <div>
+                11 rue du Panier <br>
+                13002 Marseille
+            </div>
+        </section>
     </footer>
 </body>
 </html>
