e84bfa55fc22add9422362917147c099148f53d3
Your Name <marvin.delansorne@laplateforme.io>
2025-02-04 09:41:20 +0100

    mise à jour du code de la connexion à la base de données pour le déploiement sur plesk

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -2,8 +2,9 @@
 // Informations de connexion
 $host = 'localhost';
 $dbname = 'marvin-delansorne_gestionnaire_de_menu'; // Assurez-vous que cette base existe
-$username = 'marvin-delansorne'; // Par défaut pour phpMyAdmin
+$username = 'marvindelansorne'; // Par défaut pour phpMyAdmin
 $password = 'K~5q23q4h'; // Mot de passe local (changez-le si nécessaire)
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
